74f96d85056f45fe11378311fc9e139da63ab2b7
Ghimirevivek <stephengmre@gmail.com>
2023-03-07 17:52:38 +0530

    answer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
 
-
-
+const root = document.getElementById('root')
+ReactDOM.render(<App />, root)
